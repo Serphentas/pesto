1aa18941fb56ed960a031b32182094bee00d2bda
Serphentas <Serphentas@users.noreply.github.com>
2017-03-16 01:03:56 +0100

    added main class

---
+++ src/pesto/Main.java
@@ -0,0 +1,14 @@
+package pesto;
+
+import pesto.visual.DefaultFrame;
+
+/**
+ * Starting point of the application
+ *
+ * @author Serphentas
+ */
+public class Main {
+    public static void main(String args[]) {
+      DefaultFrame.main(args);
+    }
+}
